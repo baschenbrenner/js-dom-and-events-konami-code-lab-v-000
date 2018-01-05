7a64c45c6a4bbc713cdfafe81d9c6e2801c8a712
baschenbrenner <baschenbrenner@hotmail.com>
2018-01-05 09:30:05 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,35 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let index = 0;
 function init() {
+
+  // document.body.addEventListener('keydown', onKeyDownHandler)
+
+  const main = document.body
+  main.addEventListener('keydown', onKeyDownHandler)
   // Write your JavaScript code inside the init() function
 
+  // Keep track of index outside of the event handler.
+
+
+  // This is the function that would be invoked by the event listener.
+
+}
+
+
+
+function onKeyDownHandler(e) {
+
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
